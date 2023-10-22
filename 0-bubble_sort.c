005ed90787cefc61354b67c83b678068929db4b5
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-22 22:55:36 +0400

    taskh

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,20 +24,25 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j;
 	int swapped = 0;
 
-	for (i = 0; i < size - 1; i++)
+	if (size < 2)
+	print_array(array, size);
+	else
 	{
-		swapped = 0;
-
-		for (j = 0; j < size - i - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			swapped = 0;
+
+			for (j = 0; j < size - i - 1; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				swapped = 1;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					swapped = 1;
+					print_array(array, size);
+				}
 			}
+			if (swapped == 0)
+			break;
 		}
-		if (swapped == 0)
-		break;
 	}
 }
