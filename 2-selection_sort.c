f9e4328ba94c1dc6ae83b3e409debb4e8e679908
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-24 22:53:17 +0400

    h

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -32,15 +32,15 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = &array[i];
+		min = array + i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < *min)
-			min = &array[j];
+			min = array + j;
 		}
 
-		if (min != &array[i])
-		swap(min, &array[i]);
+		if (min != (array + i))
+		swap(min, array + i);
 		print_array(array, size);
 
 	}
