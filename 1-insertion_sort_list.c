f49524cb5b9f0c9b10a1845378e80bd1e5eadf64
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-25 06:22:29 +0400

    g

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,24 +1,29 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * swap_nodes - Swap two nodes in a doubly linked list.
+ * @h: head pointer
+ * @node1: pointer to first node
+ * @node2: pointer to second node
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **h, listint_t **node1, listint_t *node2)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*node1)->next = node2->next;
+
+	if (node2->next != NULL)
+	node2->next->prev = *node1;
+
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+
+	if ((*node1)->prev != NULL)
+	(*node1)->prev->next = node2;
+
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+	*h = node2;
+
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
@@ -30,18 +35,19 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *temp, *iteration, *insertNode;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+	return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (iteration = (*list)->next; iteration != NULL; iteration = temp)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		temp = iteration->next;
+		insertNode = iteration->prev;
+
+		while (insertNode != NULL && (iteration->n < insertNode->n))
 		{
-			swap_nodes(list, &insert, iter);
+			swap_nodes(list, &insertNode, iteration);
 			print_list((const listint_t *)*list);
 		}
 	}
